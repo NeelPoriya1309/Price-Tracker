d602fae352f22889ef430445c001fe459b2a2f23
Neel Poriya <202001139@daiict.ac.in>
2022-11-03 09:47:16 +0530

    Bug in Products API

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,10 +1,10 @@
-import query from "../../../Query";
+import client from 'Query'
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    const data = await query("select * from public.product");
-    res.status(200).json({ message: "ok", body: data });
+  if (req.method === 'GET') {
+    const data = await client.query('select * from public.product')
+    res.status(200).json({ message: 'ok', body: data })
   } else {
-    res.status(404).json({ message: "invalid request!" });
+    res.status(404).json({ message: 'invalid request!' })
   }
 }
