185c1a6cba6349befba0d8c22903f9d71ff27a5f
Neel Poriya <202001139@daiict.ac.in>
2022-11-16 09:39:01 +0530

    Fixed CSS of Product Details page

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const dev = false;
+const dev = true;
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://price-tracker-seven.vercel.app';
+  : 'https://price-tracker-dbms.vercel.app';
